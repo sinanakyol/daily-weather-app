08e7aeee3eb21a42c4003f736cfc7a3a7db683bf
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-12-16 02:49:04 +0300

    files changed

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Days from "./Days";
 import axios from "axios";
 
 function Weather() {
-  const [option, setOption] = useState("");
+  const [option, setOption] = useState("denizli");
 
   const [weather, setWeather] = useState({});
 
@@ -10,31 +11,122 @@ function Weather() {
     setOption(e.target.value);
   };
 
-  {
+  useEffect(() => {
     axios(
       `https://api.openweathermap.org/data/2.5/forecast?q=${option}&units=metric&appid=b2d6ba9c81dc2eefa64c2e2586f617c7`
     )
       .then((res) => res.data)
-      .then((result) => console.log(result));
-  }
+      .then((result) => setWeather(result))
+      .catch((e) => console.log(e));
+  }, [option]);
 
-  console.log({ option });
+  console.log({ weather });
+
   return (
-    <div>
-      <select name="" id="" onChange={handleChange}>
-        <option value="İstanbul">İstanbul</option>
-        <option value="Denizli">Denizli</option>
-        <option value="Ankara">Ankara</option>
-      </select>
+    <div className="days">
+      <div className="day today">
+        <select name="" id="" onChange={handleChange}>
+          <option value="İstanbul">İstanbul</option>
+          <option value="Denizli">Denizli</option>
+          <option value="Ankara">Ankara</option>
+        </select>
 
-      <br />
-      <br />
+        <br />
+        <br />
+        <div>
+          <p>
+            <Days />
+          </p>
 
-      <div>
-        <p>Mon</p>
-        <img src="" alt="icon" />
-        <p>temp</p>
-        {/* {weather.name != "undefined" ? <p>{weather.name}</p> : ""} */}
+          {/* icon */}
+          {typeof weather.list !== "undefined" ? (
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`}
+              alt="icon"
+            />
+          ) : (
+            ""
+          )}
+
+          {/* temp */}
+          {typeof weather.list !== "undefined" ? (
+            <p className="temp">{weather.list[0].main.temp} °C </p>
+          ) : (
+            ""
+          )}
+          <br />
+
+          {/* temp_max-min */}
+          {typeof weather.list !== "undefined" ? (
+            <p>
+              {weather.list[0].main.temp_max} °C /{" "}
+              {weather.list[0].main.temp_min} °C
+            </p>
+          ) : (
+            ""
+          )}
+          <br />
+
+          {/* {name} */}
+
+          {typeof weather.city !== "undefined" ? (
+            <p>{weather.city.name}</p>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+      <div className="day">
+        <select name="" id="" onChange={handleChange}>
+          <option value="İstanbul">İstanbul</option>
+          <option value="Denizli">Denizli</option>
+          <option value="Ankara">Ankara</option>
+        </select>
+
+        <br />
+        <br />
+        <div>
+          <p>
+            <Days />
+          </p>
+
+          {/* icon */}
+          {typeof weather.list !== "undefined" ? (
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`}
+              alt="icon"
+            />
+          ) : (
+            ""
+          )}
+
+          {/* temp */}
+          {typeof weather.list !== "undefined" ? (
+            <p className="temp">{weather.list[0].main.temp} °C </p>
+          ) : (
+            ""
+          )}
+          <br />
+
+          {/* temp_max-min */}
+          {typeof weather.list !== "undefined" ? (
+            <p>
+              {weather.list[0].main.temp_max} °C /{" "}
+              {weather.list[0].main.temp_min} °C
+            </p>
+          ) : (
+            ""
+          )}
+          <br />
+
+          {/* {name} */}
+
+          {typeof weather.city !== "undefined" ? (
+            <p>{weather.city.name}</p>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
